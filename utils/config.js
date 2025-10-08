e13fd6485d9a57b793951033fad687261a18c3a3
Alejandro Jimenez <ajjime11@asu.edu>
2025-10-08 06:06:49 -1000

    Add new and improved secret key

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,6 @@
-const JWT_SECRET = "your-secret-key";
-
 module.exports = {
-  JWT_SECRET,
+  JWT_SECRET:
+    process.env.JWT_SECRET ||
+    "616818acb01ea5a076fecbe8e8b238f453d37193eda5def96a31f1c9bae7f21a",
+  NODE_ENV: process.env.NODE_ENV || "development",
 };
